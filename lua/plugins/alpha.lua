7d0019e92b64af1277ddb21bbc62071410da6506
BoulilaAnis <boulilaa247@gmail.com>
2024-12-25 14:40:06 +0100

    first commit

---
+++ lua/plugins/alpha.lua
@@ -0,0 +1,29 @@
+return {
+  'goolord/alpha-nvim',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+
+  config = function()
+    local alpha = require 'alpha'
+    local dashboard = require 'alpha.themes.dashboard'
+
+    dashboard.section.header.val = {
+      [[                                                    ]],
+      [[ ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ]],
+      [[ ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ]],
+      [[ ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ]],
+      [[ ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ]],
+      [[ ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ]],
+      [[ ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ]],
+      [[                                                    ]],
+    }
+    dashboard.section.buttons.val = {
+      dashboard.button('f', ' ' .. ' Find file', '<cmd>Telescope find_files<cr>'),
+      dashboard.button('n', ' ' .. ' New file', '<cmd>enew<cr>'),
+      dashboard.button('g', ' ' .. ' Find text', '<cmd> Telescope live_grep <cr>'),
+      dashboard.button('q', ' ' .. ' Quit', '<cmd> qa <cr>'),
+    }
+    alpha.setup(dashboard.opts)
+  end,
+}
